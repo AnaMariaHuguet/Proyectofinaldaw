b6307574385770c121af6956d90aee8e1c7f3857
unknown <anamaria.huguet.alum@iescamp.es>
2021-06-03 23:20:40 +0200

    Intento

--- src/main/java/com/proyectofinal/daw/controllers/PerfilController.java
+++ src/main/java/com/proyectofinal/daw/controllers/PerfilController.java
@@ -31,6 +31,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Controller
 public class PerfilController {
